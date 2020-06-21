9f7d36f8c2844a09232fb076b50db26c1ca73693
Deepika Tiwari <deepikatiwari92@gmail.com>
2020-06-21 23:22:34 +0200

    Instrument some more methods

---
+++ src/main/java/org/example/instrumentation/myplugin/PureAspect21.java
@@ -0,0 +1,61 @@
+package org.example.instrumentation.myplugin;
+
+import com.thoughtworks.xstream.XStream;
+import org.glowroot.agent.plugin.api.*;
+import org.glowroot.agent.plugin.api.weaving.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class PureAspect21 {
+    @Pointcut(className = "com.turn.ttorrent.common.TorrentMetadataImpl", methodName = "getAnnounceList",
+            methodParameterTypes = {}, timerName = "announce list")
+    public static class PureMethodAdvice {
+
+        private static final TimerName timer = Agent.getTimerName(PureMethodAdvice.class);
+        private static final String transactionType = "Pure";
+        private static Logger logger = Logger.getLogger(PureMethodAdvice.class);
+        private static XStream xStream = new XStream();
+        private static final String receivingObjectFilePath = "/home/user/object-data/21-receiving.xml";
+        private static final String returnedObjectFilePath = "/home/user/object-data/21-returned.xml";
+
+        public static void writeObjectXMLToFile(Object objectToWrite, String objectFilePath) {
+            try {
+                FileWriter objectFileWriter = new FileWriter(objectFilePath, true);
+                xStream.toXML(objectToWrite, objectFileWriter);
+                BufferedWriter bw = new BufferedWriter(objectFileWriter);
+                bw.newLine();
+                bw.flush();
+                bw.close();
+            } catch (Exception e) {
+                logger.info("PureAspect21");
+            }
+        }
+
+        @OnBefore
+        public static TraceEntry onBefore(OptionalThreadContext context,
+                                          @BindReceiver Object receivingObject,
+                                          @BindMethodName String methodName) {
+            writeObjectXMLToFile(receivingObject, receivingObjectFilePath);
+            MessageSupplier messageSupplier = MessageSupplier.create(
+                    "className: {}, methodName: {}",
+                    PureMethodAdvice.class.getAnnotation(Pointcut.class).className(),
+                    methodName
+            );
+            return context.startTransaction(transactionType, methodName, messageSupplier, timer, OptionalThreadContext.AlreadyInTransactionBehavior.CAPTURE_NEW_TRANSACTION);
+        }
+
+        @OnReturn
+        public static void onReturn(@BindReturn Object returnedObject,
+                                    @BindTraveler TraceEntry traceEntry) {
+            writeObjectXMLToFile(returnedObject, returnedObjectFilePath);
+            traceEntry.end();
+        }
+
+        @OnThrow
+        public static void onThrow(@BindThrowable Throwable throwable,
+                                   @BindTraveler TraceEntry traceEntry) {
+            traceEntry.endWithError(throwable);
+        }
+    }
+}
---
+++ src/main/java/org/example/instrumentation/myplugin/PureAspect25.java
@@ -0,0 +1,64 @@
+package org.example.instrumentation.myplugin;
+
+import com.thoughtworks.xstream.XStream;
+import org.glowroot.agent.plugin.api.*;
+import org.glowroot.agent.plugin.api.weaving.*;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+public class PureAspect25 {
+    @Pointcut(className = "com.turn.ttorrent.network.ConnectionWorker", methodName = "getDefaultWriteErrorMessageWithSuffix",
+            methodParameterTypes = {"java.nio.channels.SocketChannel", "java.lang.String"}, timerName = "default error message")
+    public static class PureMethodAdvice {
+
+        private static final TimerName timer = Agent.getTimerName(PureMethodAdvice.class);
+        private static final String transactionType = "Pure";
+        private static Logger logger = Logger.getLogger(PureMethodAdvice.class);
+        private static XStream xStream = new XStream();
+        private static final String receivingObjectFilePath = "/home/user/object-data/25-receiving.xml";
+        private static final String parameterObjectsFilePath = "/home/user/object-data/25-param.xml";
+        private static final String returnedObjectFilePath = "/home/user/object-data/25-returned.xml";
+
+        public static void writeObjectXMLToFile(Object objectToWrite, String objectFilePath) {
+            try {
+                FileWriter objectFileWriter = new FileWriter(objectFilePath, true);
+                xStream.toXML(objectToWrite, objectFileWriter);
+                BufferedWriter bw = new BufferedWriter(objectFileWriter);
+                bw.newLine();
+                bw.flush();
+                bw.close();
+            } catch (Exception e) {
+                logger.info("PureAspect25");
+            }
+        }
+
+        @OnBefore
+        public static TraceEntry onBefore(OptionalThreadContext context,
+                                          @BindReceiver Object receivingObject,
+                                          @BindParameterArray Object[] parameterObjects,
+                                          @BindMethodName String methodName) {
+            writeObjectXMLToFile(receivingObject, receivingObjectFilePath);
+            writeObjectXMLToFile(parameterObjects, parameterObjectsFilePath);
+            MessageSupplier messageSupplier = MessageSupplier.create(
+                    "className: {}, methodName: {}",
+                    PureMethodAdvice.class.getAnnotation(Pointcut.class).className(),
+                    methodName
+            );
+            return context.startTransaction(transactionType, methodName, messageSupplier, timer, OptionalThreadContext.AlreadyInTransactionBehavior.CAPTURE_NEW_TRANSACTION);
+        }
+
+        @OnReturn
+        public static void onReturn(@BindReturn Object returnedObject,
+                                    @BindTraveler TraceEntry traceEntry) {
+            writeObjectXMLToFile(returnedObject, returnedObjectFilePath);
+            traceEntry.end();
+        }
+
+        @OnThrow
+        public static void onThrow(@BindThrowable Throwable throwable,
+                                   @BindTraveler TraceEntry traceEntry) {
+            traceEntry.endWithError(throwable);
+        }
+    }
+}
